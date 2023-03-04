0df47879929c387fca413a783457f45835c4789e
Mayk Alexander <38488700+Maayk@users.noreply.github.com>
2023-03-04 12:52:05 -0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+client_scripts {
+    "notify.lua"
+}
+
+ui_page "app/main.html"
+
+files {
+    "app/main.html",
+    "app/notify.css",
+    "app/Roboto.ttf",
+    "app/notify.js",
+}
---
+++ notify.lua
@@ -0,0 +1,21 @@
+RegisterNetEvent('reborn:notify:send')
+AddEventHandler('reborn:notify:send', function(ntitulo,nmensagem,ntipo,ntempo)
+     local tipo = {
+          sucesso = "fa-check",
+          trofeu = "fa-trophy",
+          erro = "fa-times",
+          info = "fa-exclamation",
+          semstress = "fa-smile-wink",
+          comstress = "fa-tired",
+          vida = "fa-heartbeat",
+          gps = "fa-location-arrow",
+     }
+	SendNUIMessage({
+          style = ntipo,
+		titulo = ntitulo,
+		mensagem = nmensagem,
+          tipo = tipo[ntipo],
+		tempo = ntempo
+	})
+end)
+--xD
